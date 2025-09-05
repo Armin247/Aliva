1fc7d73b9f481bafa1091c0a346834f38631acce
Armin247 <mohammedodunlami.com>
2025-09-06 00:02:18 +0100

    redundancy

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -49,9 +49,6 @@ const HeroSection = () => {
               <MessageCircle className="w-5 h-5 mr-2 group-hover:rotate-12 transition-transform" />
               Start AI Consultation
             </Button>
-            <Button variant="outline" size="xl" className="bg-white/10 border-white/30 text-white hover:bg-white/20">
-              Watch Demo
-            </Button>
           </div>
 
           {/* Stats */}
